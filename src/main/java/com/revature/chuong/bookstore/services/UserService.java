b3c54cac00935f1fdb899aba6f52c32a1247c417
John <nguyen8585@gmail.com>
2022-08-16 11:17:05 -0700

    update

--- src/main/java/com/revature/chuong/bookstore/services/UserService.java
+++ src/main/java/com/revature/chuong/bookstore/services/UserService.java
@@ -14,11 +14,13 @@ public class UserService {
     }
 
     public void isValidUsername(String username) {
-        if (!username.matches("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$")) throw new InvalidUserException("\nInvalid username! username is 8-20 characters long. no _ or . at the beginning. no __ or _. or ._ or .. inside");
+        if (!username.matches("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$"))
+            throw new InvalidUserException("\nInvalid username! username is 8-20 characters long. no _ or . at the beginning. no __ or _. or ._ or .. inside");
     }
 
     public void isValidPassword(String password) {
-        if (!password.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$")) throw new InvalidUserException("\nInvalid password! Minimum eight characters, at least one letter and one number");
+        if (!password.matches("^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$"))
+            throw new InvalidUserException("\nInvalid password! Minimum eight characters, at least one letter and one number");
     }
 
     public void register(User user) {
